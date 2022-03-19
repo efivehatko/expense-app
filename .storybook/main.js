5814ce33e039188867a68d78bbe2617f24b20eb1
Elijah <elijah.pyatikhatko@gmail.com>
2022-03-19 21:44:12 +0200

    https://github.com/storybookjs/storybook/issues/17049#issuecomment-1000159885

--- .storybook/main.js
+++ .storybook/main.js
@@ -13,4 +13,12 @@ module.exports = {
     core: {
         builder: 'webpack5',
     },
+    webpackFinal: async (config, { configType }) => {
+        // Make whatever fine-grained changes you need
+        // Return the altered config
+
+        const ruleWithError = config.module.rules[4].include
+        config.module.rules[4].include = ruleWithError.filter((s) => !!s)
+        return config
+    },
 }
